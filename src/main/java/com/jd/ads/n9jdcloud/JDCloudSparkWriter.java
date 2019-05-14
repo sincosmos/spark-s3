1b0316e083e92bb3956dc505715abcf21b72b4c5
guankangle <guankangle@jd.com>
2019-05-14 17:38:31 +0800

    use real data

--- src/main/java/com/jd/ads/n9jdcloud/JDCloudSparkWriter.java
+++ src/main/java/com/jd/ads/n9jdcloud/JDCloudSparkWriter.java
@@ -1,7 +1,9 @@
 package com.jd.ads.n9jdcloud;
 
-import org.apache.spark.SparkContext;
-import org.apache.spark.sql.*;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.RowFactory;
+import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.types.DataTypes;
 import org.apache.spark.sql.types.StructField;
 import org.apache.spark.sql.types.StructType;
