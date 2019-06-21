75987eba611be098cb3c3a51b33ba766dc25e511
guankangle <guankangle@jd.com>
2019-06-21 10:23:52 +0800

    spark provided

--- src/main/java/com/jd/ads/n9jdcloud/JDCloudJavaRW.java
+++ src/main/java/com/jd/ads/n9jdcloud/JDCloudJavaRW.java
@@ -4,9 +4,9 @@ import com.amazonaws.AmazonServiceException;
 import com.amazonaws.ClientConfiguration;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSCredentialsProvider;
-//import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
-//import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.*;
@@ -19,7 +19,7 @@ import java.util.ResourceBundle;
 public class JDCloudJavaRW {
     private static final ResourceBundle resources = ResourceBundle.getBundle("config");
 
-    /*public static void main(String[] args){
+    public static void main(String[] args){
         JDCloudJavaRW s3Rw = new JDCloudJavaRW();
         AmazonS3 s3 = s3Rw.amazonS3();
         s3Rw.listS3Object(s3);
@@ -106,5 +106,5 @@ public class JDCloudJavaRW {
         }
         System.out.println("Done!");
 
-    }*/
+    }
 }
